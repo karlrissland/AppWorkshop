b83c4e62dc8c11b9d5d5e27610d670d817cdf452
Karl Rissland <karl_rissland@hotmail.com>
2017-09-12 12:02:22 -0400

    Added Parts Unlimited Web
    Related Work Items: #2841

---
+++ IaaS2PaaSWeb/PartsUnlimitedWebsite/api/RaincheckController.cs
@@ -0,0 +1,38 @@
+﻿using PartsUnlimited.Models;
+using PartsUnlimited.Utils;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace PartsUnlimited.Api
+{
+    [System.Web.Mvc.RoutePrefix("api/raincheck")]
+    public class RaincheckController : ApiController
+    {
+        private readonly IRaincheckQuery _query;
+
+        public RaincheckController(IRaincheckQuery query)
+        {
+            _query = query;
+        }
+
+        [System.Web.Http.HttpGet, System.Web.Mvc.Route]
+        public Task<IEnumerable<Raincheck>> Get()
+        {
+            return _query.GetAllAsync();
+        }
+
+        [System.Web.Http.HttpGet, System.Web.Mvc.Route("{id}")]
+        public Task<Raincheck> Get(int id)
+        {
+            return _query.FindAsync(id); 
+        }
+
+        [System.Web.Http.HttpPost, System.Web.Mvc.Route]
+        public Task<int> Post([FromBody]Raincheck raincheck)
+        {
+            return _query.AddAsync(raincheck);
+        }
+    }
+}

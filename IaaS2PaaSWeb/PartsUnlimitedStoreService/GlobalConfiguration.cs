f40eec17e51392486c6bddad015ba2f4201170ea
Roberto Mardeni <rmardeni@gmail.com>
2018-03-16 15:31:24 -0400

    Added PartsUnlimitedStoreService

---
+++ IaaS2PaaSWeb/PartsUnlimitedStoreService/GlobalConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+
+namespace PartsUnlimitedStoreService
+{
+    internal static class GlobalConfiguration
+    {
+        internal static void SetConfiguration(HttpRequestMessage req)
+        {
+            HttpConfiguration config = req.GetConfiguration();
+
+            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+        }
+    }
+}

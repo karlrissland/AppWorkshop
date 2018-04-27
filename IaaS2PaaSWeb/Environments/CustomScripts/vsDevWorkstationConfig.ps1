e25393d2d075d8f7e489798fb487067994106dea
Roberto Mardeni <rmardeni@gmail.com>
2018-04-27 14:58:25 -0400

    Added installation of Chocolatey and Postman in Dev VM

--- IaaS2PaaSWeb/Environments/CustomScripts/vsDevWorkstationConfig.ps1
+++ IaaS2PaaSWeb/Environments/CustomScripts/vsDevWorkstationConfig.ps1
@@ -81,3 +81,8 @@ $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A
 $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
 Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
 Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+
+#Install Chocolatey and packages
+Invoke-Expression ((New-Object Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) 
+choco feature enable -n allowGlobalConfirmation
+choco install postman -y

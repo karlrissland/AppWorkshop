0939984cc89fbb5b07f589df015bbd9b5b217231
Roberto Mardeni <rmardeni@gmail.com>
2018-07-24 14:03:50 -0400

    Additional changes to fix deployment issues

--- IaaS2PaaSWeb/Environments/CustomScripts/vsDevWorkstationConfig.ps1
+++ IaaS2PaaSWeb/Environments/CustomScripts/vsDevWorkstationConfig.ps1
@@ -11,35 +11,40 @@ Param (
 	[string]$dbUserPassword
 )
 
-#Clone Repo
-mkdir 'c:\Source'
-cd 'c:\Source'
-git clone $repoUri
+$current_dir = $PSScriptRoot
+if (!$current_dir) { $current_dir = Get-Location }
+#Stop on any error
+$ErrorActionPreference = "Stop"
 
-#Install Chocolatey and packages
+## Clone Repo
+$repo_path = 'c:\Source'
+if (!(Test-Path $repo_path)) { 
+    New-Item -Path $repo_path 
+    cd $repo_path
+    if (!(Test-Path "$repo_path\AppWorkshop")) {
+        git clone $repoUri
+    }
+}
+
+## Install Chocolatey and packages
 Invoke-Expression ((New-Object Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) 
-Start-Sleep -Seconds 5
 #Install NuGet
-& choco install -y nuget.commandline 
+& choco install nuget.commandline -y
 
-#Build and Package App
+## Build and Package App
 
 #Restore NuGet packages
 nuget restore C:\Source\AppWorkshop\IaaS2PaaSWeb\IaaS2PaaSWeb.sln
 
 #Build App using VS Tools
-Start-Sleep -Seconds 5
-$current_dir = $PSScriptRoot
-if (!$current_dir) { $current_dir = Get-Location }
-$build_bat_file = Join-Path -Path $current_dir -ChildPath "doBuild.bat"
+$build_bat_file = Join-Path -Path $current_dir -ChildPath "build.bat"
 if (!(Test-Path $build_bat_file)) {
     Add-Content -Path $build_bat_file -Value "call `"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\vsdevcmd\core\vsdevcmd_start.bat`""
     Add-Content -Path $build_bat_file -Value "call `"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\vsdevcmd\core\dotnet.bat`""
     Add-Content -Path $build_bat_file -Value "call `"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\vsdevcmd\core\msbuild.bat`""
-    Add-Content -Path $build_bat_file -Value "msbuild `"C:\Source\AppWorkshop\IaaS2PaaSWeb\PartsUnlimitedWebsite\partsunlimitedwebsite.csproj`" /p:DeployOnBuild=true /p:WebPublishMethod=Package /p:PackageAsSingleFile=true /p:SkipInvalidConfigurations=true"
+    Add-Content -Path $build_bat_file -Value "msbuild `"C:\Source\AppWorkshop\IaaS2PaaSWeb\PartsUnlimitedWebsite\PartsUnlimitedWebsite.csproj`" /p:DeployOnBuild=true /p:WebPublishMethod=Package /p:PackageAsSingleFile=true /p:SkipInvalidConfigurations=true"
 }
-cmd.exe /c $build_bat_file
-Remove-Item -Path $build_bat_file
+cmd.exe /c $build_bat_file > build.log
 
 ## Deploy Webapp
 
@@ -49,17 +54,12 @@ $SettingsFile += "<parameters>"
 $SettingsFile += "<setParameter name=""IIS Web Application Name"" value=""Default Web Site"" />"
 $SettingsFile += "<setParameter name=""DefaultConnectionString-Web.config Connection String"" value=""Server=" + $dbSrvUri + ";Database=" + $dbName + "; User Id=" + $dbUserName + "; password= " + $dbUserPassword + """/>"
 $SettingsFile += "</parameters>"
-
 Set-Content -Path C:\Source\AppWorkshop\IaaS2PaaSWeb\PartsUnlimitedWebsite\obj\Debug\Package\partsunlimitedwebsite.SetParameters.xml -Value $SettingsFile
 
 #Deploy Website
 C:\Source\AppWorkshop\IaaS2PaaSWeb\PartsUnlimitedWebsite\obj\Debug\Package\partsunlimitedwebsite.deploy.cmd /Y /M:$webSrvUri/MSDeployAgentService /U:$adminUserName /P:$adminUserPassword
 
-#Add startup bat to install additional packages on sign in
+## Add startup bat to install additional packages on sign in
 $choco_exe = "C:\ProgramData\chocolatey\bin\choco.exe"
 $install_packages_bat = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\install_packages.bat"
-@('postman') | ForEach-Object {
-	Add-Content -Path $install_packages_bat -Value "$choco_exe install -y $_"
-}
-#Install Google Chrome browser
-& choco install -y googlechrome
+Set-Content -Path $install_packages_bat -Value "$choco_exe install postman googlechrome -y"

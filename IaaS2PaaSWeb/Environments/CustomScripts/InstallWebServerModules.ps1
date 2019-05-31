a61f9a9bef0eced10614e86cc074c4b364f85ff6
Roberto Mardeni <rmardeni@gmail.com>
2019-05-31 14:43:17 -0400

    Added scripts to disable Internet Explorer ESC

--- IaaS2PaaSWeb/Environments/CustomScripts/InstallWebServerModules.ps1
+++ IaaS2PaaSWeb/Environments/CustomScripts/InstallWebServerModules.ps1
@@ -3,3 +3,14 @@ Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 
 # Install required PowerShell Modules
 Install-Module -Name cChoco -Force
+
+function Disable-InternetExplorerESC {
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0 -Force
+    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0 -Force
+    Stop-Process -Name Explorer -Force
+    Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
+}
+
+Disable-InternetExplorerESC
